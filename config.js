42ac3c70d83fdc939ef7ef71c318a722d3ef6f4e
itswhaa <102423745+itswhaa@users.noreply.github.com>
2026-02-14 00:24:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -70,7 +70,7 @@ const CONFIG = {
         floatDuration: "15s",           // How long it takes hearts to float up (10-20s recommended)
         floatDistance: "50px",          // How far hearts move sideways (30-70px recommended)
         bounceSpeed: "0.5s",            // Speed of bouncing animations (0.3-0.7s recommended)
-        heartExplosionSize: 1.5         // Size of heart explosion effect (1.2-2.0 recommended)
+        heartExplosionSize: 1.1         // Size of heart explosion effect (1.2-2.0 recommended)
     },
 
     // Background Music (Optional)
